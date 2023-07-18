aab44bdd4d2851095978561851ddfcb5cc05f652
anuparashar0507 <anuparashar0507@gmail.com>
2023-07-19 00:14:08 +0530

    Fixing Internet issue

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NativeBaseProvider, Spinner } from "native-base";
 import { theme } from "./lib/theme";
+import * as Network from "expo-network";
+import { Platform } from "react-native";
 
 // import { useColorScheme } from "react-native";
 // redux & persister
@@ -9,6 +12,7 @@ import { Provider as ReduxProvider } from "react-redux";
 import { store, persistor } from "./lib/redux/store";
 import { PersistGate } from "redux-persist/es/integration/react";
 import { LinearGradient } from "expo-linear-gradient";
+import * as Updates from "expo-updates";
 import { Root } from "./root";
 const config = {
   dependencies: {
@@ -16,6 +20,41 @@ const config = {
   },
 };
 export default function App() {
+  // const [isConnected, setIsConnected] = useState(false);
+  async function onFetchUpdateAsync() {
+    const { isConnected } = await Network.getNetworkStateAsync();
+    if (!isConnected) {
+      alert(`No Internet Connection`);
+    } else {
+      try {
+        const update = await Updates.checkForUpdateAsync();
+
+        if (update.isAvailable) {
+          alert(`New Update is Available Downloading now...`);
+          await Updates.fetchUpdateAsync();
+          await Updates.reloadAsync();
+        }
+      } catch (error) {
+        // You can also add an alert() to see the error message in case of an error when fetching updates.
+        alert(`Error fetching latest Expo update: ${error}`);
+      }
+    }
+  }
+
+  if (Platform.OS !== "web") {
+    onFetchUpdateAsync(); // Call the function on app launch
+  }
+
+  // const checkInternetConnection = async () => {
+  //   const { isConnected } = await Network.getNetworkStateAsync();
+  //   !isConnected && alert(`No Internet Connection`);
+  //   setIsConnected(isConnected);
+  // };
+
+  // useEffect(() => {
+  //   checkInternetConnection();
+  // }, []);
+
   return (
     <NativeBaseProvider config={config} theme={theme}>
       <ReduxProvider store={store}>
--- root.js
+++ root.js
@@ -1,17 +1,14 @@
 // core
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Platform } from "react-native";
-import * as Network from "expo-network";
 
 // components & theme
 import { Spinner, View } from "native-base";
-import InternetConnectionPrompt from "./components/InternetConnectionPrompt";
+// import InternetConnectionPrompt from "./components/InternetConnectionPrompt";
 
 // hooks
 import useCachedResources from "./hooks/useCachedResources";
 import useAxiosConfig from "./hooks/useAxiosConfig";
-import * as Updates from "expo-updates";
 
 // navigation
 import Navigator from "./navigation";
@@ -19,7 +16,6 @@ import Navigator from "./navigation";
 export const Root = () => {
   const isLoadingComplete = useCachedResources();
   const isAxiosSetupComplete = useAxiosConfig();
-  const [isConnected, setIsConnected] = useState(false);
 
   // const handleRetry = async () => {
   //   const { isConnected } = await Network.getNetworkStateAsync();
@@ -30,56 +26,28 @@ export const Root = () => {
   //   }
   // };
 
-  async function onFetchUpdateAsync() {
-    try {
-      const update = await Updates.checkForUpdateAsync();
-
-      if (update.isAvailable) {
-        alert(`New Update is Available Downloading now...`);
-        await Updates.fetchUpdateAsync();
-        await Updates.reloadAsync();
-      }
-    } catch (error) {
-      // You can also add an alert() to see the error message in case of an error when fetching updates.
-      alert(`Error fetching latest Expo update: ${error}`);
-    }
-  }
-
-  if (Platform.OS !== "web" && isConnected) {
-    onFetchUpdateAsync(); // Call the function on app launch
-  }
-
-  useEffect(() => {
-    const checkInternetConnection = async () => {
-      const { isConnected } = await Network.getNetworkStateAsync();
-      !isConnected && alert(`No Internet Connection`);
-      setIsConnected(isConnected);
-    };
-
-    checkInternetConnection();
-  }, []);
   return (
+    // <>
+    //   {!isConnected && Platform.OS !== "web" ? (
+    //     <InternetConnectionPrompt
+    //       setIsConnected={setIsConnected}
+    //       isConnected={isConnected}
+    //     />
+    //   ) : (
     <>
-      {!isConnected && Platform.OS !== "web" ? (
-        <InternetConnectionPrompt
-          setIsConnected={setIsConnected}
-          isConnected={isConnected}
-        />
-      ) : (
+      {!isLoadingComplete && !isAxiosSetupComplete ? (
+        <View justifyContent={"center"} alignItems={"center"} h={"full"}>
+          <Spinner alignSelf={"center"} size="lg" />
+        </View>
+      ) : null}
+      {isLoadingComplete && isAxiosSetupComplete ? (
         <>
-          {!isLoadingComplete && !isAxiosSetupComplete ? (
-            <View justifyContent={"center"} alignItems={"center"} h={"full"}>
-              <Spinner alignSelf={"center"} size="lg" />
-            </View>
-          ) : null}
-          {isLoadingComplete && isAxiosSetupComplete ? (
-            <>
-              <Navigator />
-              <StatusBar />
-            </>
-          ) : null}
+          <Navigator />
+          <StatusBar />
         </>
-      )}
+      ) : null}
     </>
+    //   )}
+    // </>
   );
 };

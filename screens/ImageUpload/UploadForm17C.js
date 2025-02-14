ad96d528e04e4848fa28d1d7c19fb5cd7635c15d
anuparashar0507 <anuparashar0507@gmail.com>
2025-02-14 14:49:14 +0530

    Fix: UPdates

--- screens/ImageUpload/UploadForm17C.js
+++ screens/ImageUpload/UploadForm17C.js
@@ -1,4 +1,5 @@
 import React, { useCallback, useState, useEffect } from "react";
+import { Camera } from "expo-camera";
 import {
   Text,
   HStack,
@@ -8,9 +9,9 @@ import {
   VStack,
   Button,
   Image,
-  Modal,
+  //   Modal,
   Box,
-  //   Divider,
+  Actionsheet,
   AlertDialog,
   Pressable,
   //   Container,
@@ -21,9 +22,10 @@ import {
   //   Heading,
 } from "native-base";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { Platform, Dimensions } from "react-native";
+import ImageModal from "react-native-image-modal";
+import { Platform, Dimensions, TouchableOpacity } from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
-import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
 import * as ImagePicker from "expo-image-picker";
 import * as FileSystem from "expo-file-system";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -34,10 +36,13 @@ const screenHeight = Dimensions.get("window").height;
 const UploadForm17C = ({ navigation: { goBack } }) => {
   const [fileList, setFileList] = useState([]);
   const [disable, setDisable] = useState(false);
-  const [isModalVisible, setIsModalVisible] = useState(false); // Modal visibility
-  const [selectedImage, setSelectedImage] = useState(null); // Image for modal
+  //   const [isModalVisible, setIsModalVisible] = useState(false); // Modal visibility
+  //   const [selectedImage, setSelectedImage] = useState(null); // Image for modal
   const [isAlertOpen, setIsAlertOpen] = useState(false); // Alert for delete confirmation
   const [imageToDelete, setImageToDelete] = useState(null); // Image selected for deletion
+  const [camera, setCamera] = useState(null);
+  //   const [isSourceAlertOpen, setIsSourceAlertOpen] = useState(false);
+  const [selectedSource, setSelectedSource] = useState(false);
   const [cameraPermission, requestCameraPermission] =
     ImagePicker.useCameraPermissions();
   const [mediaLibraryStatus, requestMediaLibraryPermission] =
@@ -114,11 +119,62 @@ const UploadForm17C = ({ navigation: { goBack } }) => {
     }
   };
 
-  // Function to handle image click for modal
-  const openModal = (image) => {
-    setSelectedImage(image); // Set the clicked image
-    setIsModalVisible(true); // Show the modal
+  //   if (!cameraPermission) {
+  //     // Camera permissions are still loading
+  //     return <View />;
+  //   }
+
+  //   if (cameraPermission !== "granted") {
+  //     // Camera permissions are not granted yet
+  //     return (
+  //       <View flex={1} justifyContent={"center"}>
+  //         <Text style={{ textAlign: "center" }}>
+  //           We need your cameraPermission to show the camera
+  //         </Text>
+  //         <Button
+  //           onPress={requestCameraPermission}
+  //           title="grant cameraPermission"
+  //         />
+  //       </View>
+  //     );
+  //   }
+
+  const captureImage = async () => {
+    if (cameraPermission === "granted") {
+      const photo = await camera.takePictureAsync({ base64: true });
+      uploadImage(photo.uri);
+    }
   };
+
+  const uploadImage = async (uri) => {
+    const response = await fetch(uri);
+    if (!response.canceled && response.assets.length > 0) {
+      const newImages = await Promise.all(
+        response.assets.map(async (asset) => {
+          const fileName = `${FileSystem.documentDirectory}${Date.now()}.jpg`;
+          await FileSystem.copyAsync({
+            from: asset.uri,
+            to: fileName,
+          });
+          return fileName;
+        })
+      );
+
+      const updatedFileList = [...fileList, ...newImages];
+      setFileList(updatedFileList);
+
+      // Save to AsyncStorage
+      await AsyncStorage.setItem(
+        "selectedImages",
+        JSON.stringify(updatedFileList)
+      );
+    }
+  };
+  // Function to handle image click for modal
+  //   const openModal = (image) => {
+  //     setSelectedImage(image); // Set the clicked image
+  //     setIsModalVisible(true); // Show the modal
+  //   };
 
   // Function to confirm delete
   const confirmDeleteImage = (image) => {
@@ -178,21 +234,11 @@ const UploadForm17C = ({ navigation: { goBack } }) => {
           space={2}
           h={screenHeight - 80}
         >
-          {/* <Container
-          // w={"full"}
-          p={5}
-          // maxH="full"
-          display={"flex"}
-          justifyContent={"center"}
-          alignItems={"center"}
-        > */}
           <VStack
             alignItems={"center"}
-            // justifyContent={"space-between"}
             h={screenHeight - 120}
             my={2}
             space={4}
-            // maxW={"full"}
             w={"full"}
           >
             {fileList.length === 0 ? (
@@ -235,7 +281,6 @@ const UploadForm17C = ({ navigation: { goBack } }) => {
                 {/* </Center> */}
               </Center>
             ) : (
-              // </HStack>
               <HStack
                 space={2}
                 m={4}
@@ -249,32 +294,71 @@ const UploadForm17C = ({ navigation: { goBack } }) => {
               >
                 {fileList.length > 0 &&
                   fileList.map((image, index) => (
-                    <Pressable
+                    <ImageModal
                       key={index}
-                      onPress={() => openModal(image)}
-                      onLongPress={() => confirmDeleteImage(image)} // Long press to delete
-                    >
-                      <Image
-                        source={{ uri: image }}
-                        alt={`Selected image ${index + 1}`}
-                        size="md"
-                        style={{
-                          width: 140,
-                          height: 200,
-                          margin: 2,
-                        }}
-                        resizeMode="contain"
-                        borderRadius={5}
-                        borderColor={"gray.300"}
-                        borderWidth={1}
-                      />
-                    </Pressable>
+                      swipeToDismiss={false}
+                      resizeMode="contain"
+                      imageBackgroundColor="#000000"
+                      style={{
+                        width: image.width,
+                        height: 100,
+                      }}
+                      source={{
+                        uri: image,
+                      }}
+                      renderHeader={() => (
+                        <HStack justifyContent={"flex-end"}>
+                          <Button
+                            onPress={() => confirmDeleteImage(image)}
+                            // variant=""
+                            _text={{ color: "red.500" }}
+                          >
+                            Delete
+                          </Button>
+                        </HStack>
+                      )}
+                      renderImageComponent={({ source, resizeMode }) => (
+                        <Image
+                          source={{ uri: source }}
+                          alt={`Selected image ${index + 1}`}
+                          size="md"
+                          style={{
+                            width: 140,
+                            height: 200,
+                            margin: 2,
+                          }}
+                          resizeMode={resizeMode}
+                          borderRadius={5}
+                          borderColor={"gray.300"}
+                          borderWidth={1}
+                        />
+                      )}
+                    />
+                    // // <Pressable
+                    // //   key={index}
+                    // //   onPress={() => openModal(image)}
+                    // //   onLongPress={() => confirmDeleteImage(image)} // Long press to delete
+                    // // >
+                    //   {/* <Image
+                    //     source={{ uri: image }}
+                    //     alt={`Selected image ${index + 1}`}
+                    //     size="md"
+                    //     style={{
+                    //       width: 140,
+                    //       height: 200,
+                    //       margin: 2,
+                    //     }}
+                    //     resizeMode="contain"
+                    //     borderRadius={5}
+                    //     borderColor={"gray.300"}
+                    //     borderWidth={1}
+                    //   /> */}
+                    // {/* </Pressable> */}
                   ))}
               </HStack>
             )}
             <Pressable
-              // key={index}
-              onPress={pickImage}
+              onPress={() => setSelectedSource(true)}
               disabled={disable}
               maxW={"xl"}
             >
@@ -305,19 +389,45 @@ const UploadForm17C = ({ navigation: { goBack } }) => {
                 </VStack>
               </Box>
             </Pressable>
-            {/* <Button
-            disabled={disable}
-            title="Pick Images from camera roll"
-            onPress={pickImage}
-          >
-            Pick Images
-          </Button> */}
+            <Actionsheet
+              isOpen={selectedSource}
+              onClose={() => setSelectedSource("false")}
+              size="full"
+            >
+              <Actionsheet.Content>
+                <Box w="100%" h={60} px={4} justifyContent="center">
+                  <Text
+                    fontSize="16"
+                    color="gray.500"
+                    _dark={{
+                      color: "gray.300",
+                    }}
+                  >
+                    Choose Option
+                  </Text>
+                </Box>
+                <Actionsheet.Item
+                  onPress={() => setSelectedSource("camera")}
+                  startIcon={
+                    <Icon as={Ionicons} size="6" name="camera-outline" />
+                  }
+                >
+                  Take Photo
+                </Actionsheet.Item>
+                <Actionsheet.Item
+                  onPress={() => pickImage()}
+                  startIcon={
+                    <Icon as={Ionicons} size="6" name="images-outline" />
+                  }
+                >
+                  Select From Gallery
+                </Actionsheet.Item>
+              </Actionsheet.Content>
+            </Actionsheet>
           </VStack>
 
-          {/* </Container> */}
-
           {/* Modal for displaying clicked image */}
-          <Modal
+          {/* <Modal
             isOpen={isModalVisible}
             onClose={() => setIsModalVisible(false)}
           >
@@ -335,7 +445,7 @@ const UploadForm17C = ({ navigation: { goBack } }) => {
                 )}
               </Modal.Body>
             </Modal.Content>
-          </Modal>
+          </Modal> */}
 
           {/* Confirmation Alert for Deleting Image */}
           <AlertDialog
@@ -358,6 +468,29 @@ const UploadForm17C = ({ navigation: { goBack } }) => {
             </AlertDialog.Content>
           </AlertDialog>
 
+          {/* <AlertDialog
+            isOpen={isSourceAlertOpen}
+            onClose={() => setIsSourceAlertOpen(false)}
+            onPointerCancel={() => setIsSourceAlertOpen(false)}
+          >
+            <AlertDialog.Content>
+              <AlertDialog.Header>Select</AlertDialog.Header>
+              <AlertDialog.Body>
+                Are you sure you want to delete this image?
+              </AlertDialog.Body>
+              <AlertDialog.Footer>
+                <Button.Group space={2}>
+                  <Button onPress={() => setSelectedSource(false)}>
+                    Cancel
+                  </Button>
+                  <Button colorScheme="danger" onPress={deleteImage}>
+                    Delete
+                  </Button>
+                </Button.Group>
+              </AlertDialog.Footer>
+            </AlertDialog.Content>
+          </AlertDialog> */}
+
           {/* {fileList.length === 0 && (
           <Center h={screenHeight - 80}>
             <Spinner size={"lg"} />
@@ -365,8 +498,73 @@ const UploadForm17C = ({ navigation: { goBack } }) => {
         )} */}
         </Flex>
       </ScrollView>
+      {selectedSource === "camera" && (
+        <View flex={1} justifyContent={"center"}>
+          <Camera
+            // style={styles.camera}
+            style={{ flex: 1 }}
+            ref={(ref) => {
+              setCamera(ref);
+            }}
+          >
+            <View
+              flex={1}
+              flexDirection={"row"}
+              justifyContent={"center"}
+              backgroundColor={"transparent"}
+              m={32}
+            >
+              <TouchableOpacity
+                style={{
+                  width: 70,
+                  height: 70,
+                  borderRadius: 35,
+                  backgroundColor: "#fff",
+                  justifyContent: "center",
+                  alignSelf: "flex-end",
+                  alignItems: "center",
+                }}
+                onPress={captureImage}
+              >
+                <MaterialCommunityIcons name="camera" size={36} color="black" />
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      )}
     </View>
   );
 };
 
 export default UploadForm17C;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//   },
+//   camera: {
+//     flex: 1,
+//   },
+//   buttonContainer: {
+//     flex: 1,
+//     flexDirection: "row",
+//     backgroundColor: "transparent",
+//     justifyContent: "center",
+//     margin: 32,
+//   },
+//   button: {
+//     width: 70,
+//     height: 70,
+//     borderRadius: 35,
+//     backgroundColor: "#fff",
+//     justifyContent: "center",
+//     alignSelf: "flex-end",
+//     alignItems: "center",
+//   },
+//   text: {
+//     fontSize: 24,
+//     fontWeight: "bold",
+//     color: "white",
+//   },
+// });
